b447e04f8bcb123463b2c0f9013cf9e4e3177432
ArletteUribe <arlette.uribe@iteso.mx>
2024-05-10 13:50:24 -0600

    Controlador

---
+++ controlador.c
@@ -0,0 +1,33 @@
+/*
+ * controlador.c
+ *
+ *  Created on: 10 may 2024
+ *
+ * @Authors: José Ángel González Vázquez
+ * 			 Arlette Gabriela Uribe Ventura
+ */
+
+
+#include "controlador.h"
+
+/******************************************************************************
+ * Global variables:
+******************************************************************************/
+
+float k_lqr[4] = {-16.6265,-16.6020,-3.2059 ,-2.1024};
+float k_AV = 0.95;
+float kT = 0.4006;
+float par_motor = 0;
+float Vin = 0;
+
+/******************************************************************************
+ * Function code:
+******************************************************************************/
+
+float LQR(float q1,float q2,float dq1,float dq2){
+
+	par_motor = -1*(k_lqr[0]*q1 + k_lqr[1]*q2 + k_lqr[2]*dq1 + k_lqr[3]*dq2);
+	Vin = (kT*k_AV)/par_motor;
+
+	return Vin;
+}
---
+++ controlador.h
@@ -0,0 +1,15 @@
+/*
+ * controlador.h
+ *
+ *  Created on: 10 may 2024
+ *
+ * @Authors: José Ángel González Vázquez
+ * 			 Arlette Gabriela Uribe Ventura
+ */
+
+#ifndef CONTROLADOR_H_
+#define CONTROLADOR_H_
+
+float LQR(float q1,float q2,float dq1,float dq2);
+
+#endif /* CONTROLADOR_H_ */
